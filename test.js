fea78239f8d2c7b031eb84a025af86d9b6d757bc
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-02-12 07:36:11 +0600

    frd problem

---
+++ test.js
@@ -0,0 +1,10 @@
+ // const placeslidt = document.getElementById("placesConataner");
+// // console.log(placeslidt);
+// const li =doucument.createElement('li');
+// li.innerText ='papa';
+// placesList.appendChild(li);
+// step--1 jata add korbo tar parant class ka maention korvo
+
+const mainContainer = document.getElementById('main-container')
+console.log(mainContainer);
+//ke add korvo ta expalyn korvo akhna::
